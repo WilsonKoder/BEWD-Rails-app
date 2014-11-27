0b9cd787793f2f13502a1dae71518b91d5fb8807
WilsonKoder111 <wilson.koder.111@gmail.com>
2014-11-27 19:56:13 +0800

    init

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Movie.create(title: "22 Jump Street", description: "The sequel to hit movie '21 Jump Street'", year_released:2014)
+Movie.create(title: "The Internets Own Boy", description: "A documentary about prodigy programmer Aaron Swartz'", year_released:2014)
+Movie.create(title: "The Pirate Bay: AFK", description: "A documentary following the creators of torrent website The Pirate Bay", year_released:2013)
+Movie.create(title: "Interstellar", description: "A movie about the earth being and the Human race struggle to find a new planet", year_released: 2014)
+Movie.create(title: "Guardians of the Galaxy", description: "Some people save the galaxy.", year_released:2014)
